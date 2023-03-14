a73e1ea555e7215ee3b7637572ed3e303a8647aa
Andy Belle-Isle <abelleisle@protonmail.com>
2023-03-13 23:30:29 -0500

    Didn't import

--- commands/leave.ts
+++ commands/leave.ts
@@ -1,4 +1,4 @@
-import { Message } from "discord.js";
+import { Message, SlashCommandBuilder } from "discord.js";
 import { bot } from "../index";
 import { i18n } from "../utils/i18n";
 import { canModifyQueue } from "../utils/queue";
