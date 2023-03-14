9c0f78c09d145d4548e19ded81cdadbe75b4d91f
Andy Belle-Isle <abelleisle@protonmail.com>
2023-03-13 23:23:45 -0500

    Added leave command

---
+++ commands/leave.ts
@@ -0,0 +1,22 @@
+import { Message } from "discord.js";
+import { bot } from "../index";
+import { i18n } from "../utils/i18n";
+import { canModifyQueue } from "../utils/queue";
+
+export default {
+    name: "leave",
+    aliases: ["dc"],
+    description: i18n.__("leave.description"),
+    execute(message: Message) {
+        const queue = bot.queues.get(message.guild!.id);
+
+        if (!queue) return message.reply(i18n.__("leave.errorNotQueue")).catch(console.error);
+        if (!canModifyQueue(message.member!)) return i18n.__("common.errorNotChannel");
+
+        queue.stop();
+
+        queue.textChannel.send(i18n.__mf("leave.result", { author: message.author })).catch(console.error);
+
+        queue.connection.destroy();
+    }
+};
